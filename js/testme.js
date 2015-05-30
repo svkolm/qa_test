dca54d705b05067dff1fbcf367b7c3dd2ab741b5
Sergei Kolm <svkolm@yandex.ru>
2015-05-30 18:38:50 +0500

    JsUsefultClass test

--- js/testme.js
+++ js/testme.js
@@ -1,35 +1,44 @@
-function JsUsefulClass(level){
-    params = [];
-    return {
+var JsUsefulClass = (function(_) {'use strict';
+  function JsUsefulClass(){
+    this.instances = {};
+  }
+
+  JsUsefulClass.prototype.add_number = function(param, level){
+    if (!this.instances[level]){
+      this.instances[level] = makeLevel(level);
+    }
+    return this.instances[level].append(param);
+
+    function makeLevel(level) {
+      var params = [];
+
+      return {
         append: function(p){
-            params.push(p);
-            return this;
+          params.push(p);
+          return this;
         },
         el_numeros: function(func){
-            var ret = [];
-            for (i in params){
-                ret.push(func?func(i, level):i % level);
-            }
-            return ret;
+          var i, ret = [];
+          for (i = 0; i < params.length; i++){
+            ret.push(func?func(i, level):i % level);
+          }
+          return ret;
         }
+      };
     }
-}
-JsUsefulClass.prototype.instances = {};
-JsUsefulClass.prototype.add_number = function(param, level){
-    if (!JsUsefulClass.instances[level]){
-        JsUsefulClass.instances[level] = JsUsefulClass(level);
-    }
-    return JsUsefulClass.instances[level].append(param);
-}
-JsUsefulClass.prototype.getAllLevels = function(func){
-    return _.reduce(this.instances, function(memo, inst){
-        memo[key] = inst.el_numeros(func);
-        return memo;
+  };
+
+  JsUsefulClass.prototype.getAllLevels = function(func){
+    return _.reduce(this.instances, function(memo, inst, key){
+      memo[key] = inst.el_numeros(func);
+      return memo;
     }, {});
-}
+  };
+  return JsUsefulClass;
+}(_));
 
 Notifier = window.Notifier || {
-    sendNotify: function(text, param){
-        console.log(text, param);
-    }
-}
+  sendNotify: function(text, param){
+    console.log(text, param);
+  }
+};
--- js/testme.test.js
+++ js/testme.test.js
@@ -1,3 +1,71 @@
-describe("unittest global", function(){
-    it("must work", function(){})
-})
+describe('Объект JsUsefulClass', function() {
+  var useful, useful2;
+
+  it('не должен иметь уровней при создании', function() {
+    useful = new JsUsefulClass();
+    expect(Object.keys(useful.instances).length).toBe(0);
+  });
+
+  it('должен добавлять новый уровень при добавлении номера', function() {
+    useful.add_number(1, 1);
+    expect(Object.keys(useful.instances).length).toBe(1);
+  });
+
+  it('должен использовать существующий уровень при добавлении номера', function() {
+    useful.add_number(2, 1);
+    expect(Object.keys(useful.instances).length).toBe(1);
+  });
+
+  it('должен возвращать соответствующий уровень при добавлении номера', function() {
+    var levelKey = 1, level;
+    level = useful.add_number(3, levelKey);
+    expect(level).toEqual(useful.instances[levelKey]);
+  });
+
+  it('должен возвращать остаток от деления номера параметра на уровень для всех параметров всех уровней', function() {
+    useful.add_number(1, 2);
+    useful.add_number(2, 2);
+    useful.add_number(3, 2);
+    expect(useful.getAllLevels()).toEqual({ 1: [ 0, 0, 0 ], 2: [ 0, 1, 0 ] });
+  });
+
+  it('должен возвращать результат функции, примененной для всех параметров всех уровней', function() {
+    expect(useful.getAllLevels(function (num, level) {
+      return num * level;
+    })).toEqual({ 1: [ 0, 1, 2 ], 2: [ 0, 2, 4 ] });
+  });
+
+  it('2 не должен иметь уровней при создании', function() {
+    useful2 = new JsUsefulClass();
+    expect(Object.keys(useful2.instances).length).toBe(0);
+  });
+
+  it('2 должен добавлять новый уровень при добавлении номера', function() {
+    useful2.add_number(1, 1);
+    expect(Object.keys(useful2.instances).length).toBe(1);
+  });
+
+  it('2 должен использовать существующий уровень при добавлении номера', function() {
+    useful2.add_number(2, 1);
+    expect(Object.keys(useful2.instances).length).toBe(1);
+  });
+
+  it('2 должен возвращать соответствующий уровень при добавлении номера', function() {
+    var levelKey = 1, level;
+    level = useful2.add_number(3, levelKey);
+    expect(level).toEqual(useful2.instances[levelKey]);
+  });
+
+  it('2 должен возвращать остаток от деления номера параметра на уровень для всех параметров всех уровней', function() {
+    useful2.add_number(1, 2);
+    useful2.add_number(2, 2);
+    useful2.add_number(3, 2);
+    expect(useful2.getAllLevels()).toEqual({ 1: [ 0, 0, 0 ], 2: [ 0, 1, 0 ] });
+  });
+
+  it('2 должен возвращать результат функции, примененной для всех параметров всех уровней', function() {
+    expect(useful2.getAllLevels(function (num, level) {
+      return num * level;
+    })).toEqual({ 1: [ 0, 1, 2 ], 2: [ 0, 2, 4 ] });
+  });
+});
